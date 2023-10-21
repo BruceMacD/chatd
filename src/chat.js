fc40e8bf7ea300351303e639807d5b71e407bf7e
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-10-21 16:38:50 -0400

    visual updates

--- src/chat.js
+++ src/chat.js
@@ -1,4 +1,5 @@
 const { openFile } = require("./service/file.js");
+const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
 
 async function sendChat(event) {
   event.reply("chat:reply", { success: true, content: "TODO" });
@@ -6,9 +7,19 @@ async function sendChat(event) {
 
 async function newChat(event) {
   try {
-    const content = await openFile();
-    console.log(content);
-    event.reply("chat:load", { success: true, content });
+    // read the document
+    const doc = await openFile();
+    console.log(doc);
+
+    // split the document date for retrieval
+    const splitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 500,
+      chunkOverlap: 50,
+    });
+    const split = await splitter.splitDocuments([doc]);
+    console.log(split);
+
+    event.reply("chat:load", { success: true, doc });
   } catch (err) {
     console.log(err);
     event.reply("chat:load", { success: false, content: err.message });
--- src/client.js
+++ src/client.js
@@ -1,19 +1,48 @@
 // This script handles interaction with the user interface, as well as communication
 // between the renderer thread (UI) and the worker thread (processing).
 
-const outputElement = document.getElementById("output");
+const userInput = document.getElementById("user-input-text");
+const historyContainer = document.getElementById("history");
+// const outputElement = document.getElementById("output");
 const openFileButton = document.getElementById("openFileBtn");
+
+userInput.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    // Disable input while processing
+    userInput.disabled = true;
+    userInput.placeholder = "";
+
+    // Create a new text block
+    const historyMessage = document.createElement("div");
+    historyMessage.className = "history-user-message";
+    historyMessage.innerText = userInput.value;
+    historyContainer.appendChild(historyMessage);
+    // Clear current input
+    userInput.value = "";
+
+    // Chat processing goes here
+    // window.electronAPI.processChat(historyMessage.innerText);
+    const response = document.createElement("div");
+    response.className = "history-chat-response";
+    response.innerText = "hi";
+    historyContainer.appendChild(response);
+
+    userInput.disabled = false;
+    userInput.focus();
+  }
+});
 
 openFileButton.addEventListener("click", () => {
   window.electronAPI.newChat();
 });
 
-window.electronAPI.onChatLoaded((event, data) => {
-  // this callback recieves file data in the renderer process
-  console.log("Chat loaded:", data);
-  if (data.success) {
-    outputElement.innerText = data.content;
-  } else {
-    outputElement.innerText = `Error starting chat: ${data.content}`;
-  }
-});
+// window.electronAPI.onChatLoaded((event, data) => {
+//   // this callback recieves file data in the renderer process
+//   console.log("Chat loaded:", data);
+//   if (data.success) {
+//     outputElement.innerText = data.content;
+//   } else {
+//     outputElement.innerText = `Error starting chat: ${data.content}`;
+//   }
+// });
