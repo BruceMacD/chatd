26539229b19dde2be75ecd2796789c7cb85b922f
Bruce MacDonald <brucewmacdonald@gmail.com>
2023-11-01 19:33:15 -0400

    Revert "do not check for updates on first run to prevent windows err"
    This reverts commit b0147119b24ac10385313ef003783e22a1d1b2eb.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const {
 const os = require("os");
 const path = require("path");
 const winston = require("winston");
-const Store = require("electron-store");
 const {
   getModel,
   setModel,
@@ -21,7 +20,6 @@ const {
   runOllamaModel,
 } = require("./chat.js");
 
-const store = new Store();
 const appVersion = app.getVersion();
 const osType = os.type(); // e.g., 'Darwin', 'Windows_NT', etc.
 const osArch = os.arch(); // e.g., 'x64', 'ia32', etc.
@@ -41,17 +39,6 @@ const logger = winston.createLogger({
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
   app.quit();
-}
-
-function isFirstRun() {
-  if (store.get("firstRun") === undefined) {
-    // If the firstRun key doesn't exist, set it as false, because this is the first run.
-    store.set("firstRun", false);
-    return true;
-  } else {
-    // If the key exists, this isn't the first run.
-    return false;
-  }
 }
 
 const createWindow = () => {
@@ -111,30 +98,28 @@ app.on("ready", () => {
       }
     }
 
-    if (!isFirstRun()) {
-      // Auto-updater logic
-      autoUpdater.setFeedURL({
-        url: updateURL,
-      });
+    // Auto-updater logic
+    autoUpdater.setFeedURL({
+      url: updateURL,
+    });
+    autoUpdater.checkForUpdates();
+
+    setInterval(() => {
       autoUpdater.checkForUpdates();
+    }, 3600000); // Check every hour
 
-      setInterval(() => {
-        autoUpdater.checkForUpdates();
-      }, 3600000); // Check every hour
+    autoUpdater.on("update-available", (info) => {
+      logger.info("Update available");
+    });
 
-      autoUpdater.on("update-available", (info) => {
-        logger.info("Update available");
-      });
+    autoUpdater.on("update-downloaded", () => {
+      // The update is ready to be installed on app restart.
+      logger.info("Update downloaded");
+    });
 
-      autoUpdater.on("update-downloaded", () => {
-        // The update is ready to be installed on app restart.
-        logger.info("Update downloaded");
-      });
-
-      autoUpdater.on("error", (err) => {
-        logger.error("Error in auto-updater: ", err);
-      });
-    }
+    autoUpdater.on("error", (err) => {
+      logger.error("Error in auto-updater: ", err);
+    });
   }
 
   createWindow();
